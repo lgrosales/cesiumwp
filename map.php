b7d423ef86e1c88a336f793724a62d7f9bc64276
Leo <leonardogrosales@gmail.com>
2015-07-06 12:33:37 +0100

    Clean up Cesium samples

--- map.php
+++ map.php
@@ -1,5 +1,26 @@
 <?php /* Template Name: Map Template */ ?>
 
-<?php get_template_part('templates/page', 'header'); ?>
+<!--scripts.php-->
+<!--wp_enqueue_style( 'cesium_widgets_style', get_template_directory_uri() . '/Cesium/Widgets/widgets.css');
+  wp_enqueue_script('cesium', get_template_directory_uri() . '/Cesium/Cesium.js', false, null, false);-->
 
-Hola
+<div id="cesiumContainer"></div>
+ <script>
+    var viewer = new Cesium.Viewer('cesiumContainer', {
+        baseLayerPicker : false,
+        geocoder : false,
+        timeline : false,
+        navigationInstructionsInitiallyVisible: true,
+        fullscreenElement : document.getElementById('cesiumContainer'),
+        imageryProvider : new Cesium.BingMapsImageryProvider({
+            url : '//dev.virtualearth.net',
+            key : 'AiL8RM2VH01_zKVjyov3I1pnjFqsyciHcCUavxknsCHOky3r7_q-uLQByl1Y_laK',
+            mapStyle : Cesium.BingMapsStyle.AERIAL_WITH_LABELS
+        }),
+    });
+    
+    viewer.dataSources.add(Cesium.CzmlDataSource.load('/wp-content/themes/dw-timeline/outforaride.czml')).then(function(dataSource){
+      viewer.selectedEntity = dataSource.entities.getById('Calgary');
+    });
+  </script>
+
